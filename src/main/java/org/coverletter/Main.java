197fb88f016ff04beb3503bc664c1cb6eb3efbb9
Samyuktha <samyuj15@gmail.com>
2023-06-12 10:33:05 +0530

    add skillsets and fix reading singleQuote in input file

--- src/main/java/org/coverletter/Main.java
+++ src/main/java/org/coverletter/Main.java
@@ -10,19 +10,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Cover Letter Builder");
-//        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("input.txt");
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        System.out.println("Cover Letter Builder\n");
+
         try {
             String input = new String(Files.readAllBytes(Paths.get("src/main/resources/input.txt")), StandardCharsets.UTF_8);
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter company name");
-            String company = scanner.next();
+            String company = scanner.nextLine();
 
             System.out.println("Enter role");
-            String role = scanner.next();
+            String role = scanner.nextLine();
 
-            String coverLetter = TemplateBuilder.generateCoverLetter(input, new String[] {company, role});
+            System.out.println("Enter skills");
+            String skills = scanner.nextLine();
+
+            String[] inputArgs = {company, role, skills};
+            String coverLetter = TemplateBuilder.generateCoverLetter(input, inputArgs);
             System.out.println(coverLetter);
 
         } catch (IOException e) {
--- src/main/java/org/coverletter/TemplateBuilder.java
+++ src/main/java/org/coverletter/TemplateBuilder.java
@@ -4,7 +4,7 @@ import java.text.MessageFormat;
 
 public class TemplateBuilder {
     static String generateCoverLetter(String input, String[] args) {
-        String coverLetterOutput = MessageFormat.format(input, args[0], args[1]);
+        String coverLetterOutput = MessageFormat.format(input.replaceAll("'","''"), args[0], args[1], args[2]);
 
         return coverLetterOutput;
     }
